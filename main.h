e331f92f76842322536ee8bb0511472e422dba01
Brian Otieno <ourranous@gmail.com>
2022-08-11 06:55:53 -0700

    new

--- main.h
+++ main.h
@@ -33,19 +33,20 @@ typedef struct format
 } format_t;
 
 /* Function Prototypes */
-
 int _putchar(char c);
 int _printf(const char *format, ...);
-/* functions to print numbers */
-int print_binary(va_list types, char buffer[], int flags, int width, int precision, int size);
-int print_unsigned(va_list types, char buffer[], int flags, int width, int precision, int size);
-int print_octal(va_list types, char buffer[], int flags, int width, int precision, int size);
-int print_hexadecimal(va_list types, char buffer[], int flags, int width, int precision, int size);
-int print_hexa_upper(va_list types, char buffer[], int flags, int width, int precision, int size);
-/* basic functions */
 int print_c(va_list parameters);
 int print_s(va_list parameters);
 int print_percent(__attribute__((unused))va_list parameters);
+int print_i(va_list parameters);
+int print_d(va_list parameters);
 int (*get_func(const char a))(va_list);
+int print_integer(va_list parameters);
+int print_decimal(va_list parameters);
+int binary_recursive(unsigned int num, int len);
+int print_binary(va_list parameters);
+int print_unint(va_list parameters);
+int print_octal(va_list args);
+int rot_13(va_list parameters);
 
 #endif
